d0e50fabdbc09e8f040b7b099ed239b098b8db93
Christian Socias <Socias_93@hotmail.com>
2026-01-22 15:56:36 +0100

    updated the list for movies

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,20 @@ function App() {
         <thead>
           <tr>
             <th scope="col">Title</th>
-            <th scope="col"></th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Popularity</th>
+            <th scope="col">Released</th>
+            <th scope="col">Raiting</th>
           </tr>
         </thead>
         <tbody>
           {movies.map((movie) => (
             <tr key={movie.id}>
               <td>{movie.title}</td>
-              <td>{movie.release_date}</td>
+              <td>{movie.popularity.toFixed()} / 900</td>
+              <td>{movie.release_date} </td>
+              <td>
+                {movie.vote_average} / 10 ({movie.vote_count} votes)
+              </td>
             </tr>
           ))}
         </tbody>
