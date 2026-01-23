17f73ab2cad0393e2020329bef12d29d8a3f68ad
Christian Socias <Socias_93@hotmail.com>
2026-01-23 14:00:34 +0100

    added navbar components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getCategories, Movies } from "../services/movieService";
 import Popular from "../components/Popular";
+import Navbar from "../components/Navbar";
 
 function HomePage() {
   const [movies, setMovies] = useState<Movies[]>([]);
@@ -8,7 +9,12 @@ function HomePage() {
   useEffect(() => {
     getCategories().then(setMovies);
   }, []);
-  return <Popular movies={movies} />;
+  return (
+    <>
+      <Navbar />
+      <Popular movies={movies} />;
+    </>
+  );
 }
 
 export default HomePage;
