f40a4fe8a1a932722eec1efa767894f4cb4ad1b9
Christian Socias <Socias_93@hotmail.com>
2026-02-01 11:14:45 +0100

    updated actors page and changed BASE_URL to be able to fetch the correct data

--- src/pages/Actors.tsx
+++ src/pages/Actors.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { Actor, getPopularActors } from "../services/actorSerive";
 function ActorsPage() {
-  return <h1>Actors</h1>;
+  const [actors, setActors] = useState<Actor[]>([]);
+
+  useEffect(() => {
+    getPopularActors().then(setActors);
+  }, []);
+
+  return (
+    <ul>
+      {actors.map((actor) => (
+        <li key={actor.id}> {actor.name} </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ActorsPage;
--- src/services/actorSerive.ts
+++ src/services/actorSerive.ts
@@ -1,5 +1,6 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = `https://api.themoviedb.org/3/trending/person?api_key=${API_KEY}`;
+const BASE_URL = `https://api.themoviedb.org/3/trending/person/day?api_key=${API_KEY}&language=en-US
+`;
 
 export interface Actor {
   id: string;
