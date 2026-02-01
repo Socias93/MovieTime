b586158bddbeaa62ec4854e39c8fc3f3714f6a14
Christian Socias <Socias_93@hotmail.com>
2026-02-01 11:04:28 +0100

    created Actor itnerface

--- src/services/actorSerive.ts
+++ src/services/actorSerive.ts
@@ -1,2 +1,9 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = `https://api.themoviedb.org/3/trending/person?api_key=${API_KEY}`;
+
+export interface Actor {
+  id: string;
+  name: string;
+  gender: number;
+  known_for_department: string;
+}
