e6a871c20f5321893edcecad8decc269f7941514
Christian Socias <Socias_93@hotmail.com>
2026-02-05 19:42:58 +0100

    fixed images of profile.path doesnt exist

--- src/pages/Actors.tsx
+++ src/pages/Actors.tsx
@@ -9,15 +9,24 @@ function ActorsPage() {
 
   return (
     <ul>
-      {actors.map((actor) => (
-        <div key={actor.id}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-            alt={actor.profile_path}
-          />
-          <li> {actor.name} </li>
-        </div>
-      ))}
+      <div className="serie-scroll-container mt-3 mb-3">
+        {actors.map((actor) => (
+          <div key={actor.id} className="serie-card">
+            <img
+              src={
+                actor.profile_path
+                  ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+                  : "/public/images/user.jpg"
+              }
+            />
+
+            <div key={actor.id + 1} className="serie-card">
+              {actor.profile_path === null}
+              <li> {actor.name} </li>
+            </div>
+          </div>
+        ))}
+      </div>
     </ul>
   );
 }
